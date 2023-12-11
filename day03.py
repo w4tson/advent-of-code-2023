ec0edd70a2a480d7e84c3c847ff7a76278bfacb0
watson <paul@hashbang.digital>
2023-12-11 07:57:30 +0000

    WIP

--- day03.py
+++ day03.py
@@ -9,8 +9,7 @@ def flatten_lists(a):
     else:
         yield a
 
-
-def day03(input):
+def calc_maps(input):
     parts_map = {}
     coords_map = {}
     for i, line in enumerate(input):
@@ -18,6 +17,11 @@ def day03(input):
         coords_map = coords_map | new_coords_map
         parts_map = parts_map | new_parts
 
+    return parts_map, coords_map
+
+
+def day03(input):
+    parts_map, coords_map = calc_maps(input)
     symbols = [k for k, v in parts_map.items() if not v.isdigit()]
 
     symbol_coords = [coord for coord, index in coords_map.items() if index in symbols]
@@ -25,6 +29,27 @@ def day03(input):
     print_map(all_coords, coords_map, parts_map)
     matched_indexes = set([coords_map[coord] for coord in all_coords if coords_map.get(coord) is not None])
     print(sum([int(parts_map[index]) for index in matched_indexes]))
+
+
+def day03_part2(input):
+    parts_map, coords_map = calc_maps(input)
+
+    stars = [k for k, v in parts_map.items() if v == '*']
+    symbols = [k for k, v in parts_map.items() if not v.isdigit()]
+
+    # all_coords = list(flatten_lists([surround_coords(*coord) for coord in stars]))
+    symbol_coords = [coord for coord, index in coords_map.items() if index in symbols]
+
+    stars_coords = [coord for coord, index in coords_map.items() if index in stars]
+    print(f"stars ={stars_coords}")
+    # print(symbol_coords)
+    all_coords = list(flatten_lists([surround_coords(*coord) for coord in symbol_coords if coordsd]))
+    print_map(all_coords, coords_map, parts_map)
+
+    for
+
+    # matched_indexes = set([coords_map[coord] for coord in all_coords if coords_map.get(coord) is not None])
+    # print(sum([int(parts_map[index]) for index in matched_indexes]))
 
 
 def print_map(all_coords, coords_map, parts_map):
@@ -90,7 +115,7 @@ def to_row(r, y_index, parts_map):
 
 if __name__ == '__main__':
     with open('inputs/day03','r') as f:
-        day03(f)
+        day03_part2(f)
 
 
 
